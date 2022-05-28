d9f94aa2555be7a4cbee9b0ef8b0beaf1223077e
seokjun <sukjune96@gmail.com>
2022-05-28 17:20:32 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Calendar from "react-calendar";
+import "./calendar.css";
+import { useState } from "react";
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>asdf</h1>
+      <Calendar locale="en-EN" />
+      {/* onClickDay */}
     </div>
   );
 }
