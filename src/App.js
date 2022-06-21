e9a139e58bb880b698756570bf2964e316f3a997
seokjun <sukjune96@gmail.com>
2022-06-21 16:47:54 +0900

    add calendar lib

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import "./calendar.css";
 import { useState } from "react";
 
 function App() {
+  const [date, setDate] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState(new Date());
+  console.log(date);
 
   return (
     <div className="App">
       <h1>asdf</h1>
-      <Calendar locale="en-EN" />
+      <Calendar
+        locale="en-EN"
+        onChange={setDate}
+        value={date}
+        minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+        maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+        navigationLabel={null}
+      />
       {/* onClickDay */}
     </div>
   );
